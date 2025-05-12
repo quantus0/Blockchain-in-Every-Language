3d490ea72758ef24a219436b29bfebb5e9f62ef8
quantus <dw052917@gmail.com>
2025-05-12 18:34:41 +0530

    Update

---
+++ script-chain.js
@@ -0,0 +1,73 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.nonce = 0;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data) +
+        this.nonce
+    ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block mined: " + this.hash);
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
+  }
+
+  createGenesisBlock() {
+    return new Block(0, Date.now(), "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const current = this.chain[i];
+      const previous = this.chain[i - 1];
+
+      if (current.hash !== current.calculateHash()) return false;
+      if (current.previousHash !== previous.hash) return false;
+    }
+    return true;
+  }
+}
+
+// Usage 
+let myCoin = new Blockchain();
+console.log("Mining block 1...");
+myCoin.addBlock(new Block(1, Date.now(), { amount: 4 }));
+console.log("Mining block 2...");
+myCoin.addBlock(new Block(2, Date.now(), { amount: 10 }));
+
+console.log(JSON.stringify(myCoin, null, 2));
